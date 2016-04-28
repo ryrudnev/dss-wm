89bcd3f949016d14c211bb7c54a72b7ea15a4fdb
Роман Руднев <ryrudnev@gmail.com>
2016-04-28 04:15:10 +0300

    Now can get only some subjects via request to Subject resource.

--- server/models/subject.storage.js
+++ server/models/subject.storage.js
@@ -232,7 +232,7 @@ class SubjectStorage extends RdfBaseStorage {
     }
   }
 
-  selectIndivids({ subtypes, byMethods, byWaste, sort, offset, limit } = {}) {
+  selectIndivids({ fids, subtypes, byMethods, byWaste, sort, offset, limit } = {}) {
     const query = `
       SELECT DISTINCT ${qFidAs('subject', 'fid')} ?title ?coordinates ?budget
       ${byMethods ? qFidAs('method', 'methodFid') : ''}
@@ -242,6 +242,7 @@ class SubjectStorage extends RdfBaseStorage {
         ${qType(['subject', 'a'], [this.entity, subtypes])}
         OPTIONAL { ?subject :coordinates ?coordinates }
         OPTIONAL { ?subject :budget ?budget }
+        ${qInFilter(['subject', void 0, void 0], fids)}
         ${qInFilter(['subject', ':hasMethod', 'method', true], byMethods)}
         ${qInFilter(['subject', ':hasWaste', 'waste', true], byWaste)}
       } ${qSort(sort)} ${qLimitOffset(limit, offset)}
